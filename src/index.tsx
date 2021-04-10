38b317a63e46773457acdf92eb28d2da6ecba095
findtravishere <traviscanbefound@gmail.com>
2021-04-10 19:19:21 +0800

    wip: user signup and login with cognito

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React, { ReactElement } from "react";
-import { Text, LoadUp } from "@components";
+import { LoadUp } from "@components";
 import Navigator from "./config/navigator";
+import Amplify from "aws-amplify";
+import awsExports from "../aws-exports";
+Amplify.configure(awsExports);
 
 export default function App(): ReactElement {
 	return (
--- src/screens/login/login.tsx
+++ src/screens/login/login.tsx
@@ -1,10 +1,80 @@
-import React, { ReactElement } from "react";
-import { View, Text } from "react-native";
+import React, { ReactElement, useState } from "react";
+import { ScrollView, TextInput, Button, Alert } from "react-native";
+import { Auth } from "aws-amplify";
+import styles from "./login.styles";
 
 export default function Login(): ReactElement {
+	const [login, setLogin] = useState({
+		username: "",
+		password: "",
+	});
+	const setLoginInput = (key: keyof typeof login, value: string) => {
+		setLogin({ ...login, [key]: value });
+	};
+
+	// const signup = async () => {
+	// 	try {
+	// 		const res = await Auth.signUp({
+	// 			username: "test",
+	// 			password: "12345678",
+	// 			attributes: {
+	// 				email: "test@test.com",
+	// 				name: "tester",
+	// 			},
+	// 		});
+	// 		console.log(res);
+	// 	} catch (err) {
+	// 		console.log(err);
+	// 	}
+	// };
+
+	const submit = async () => {
+		const { username, password } = login;
+		try {
+			const res = await Auth.signIn(username, password);
+			console.log(res);
+		} catch (err) {
+			console.log(err);
+			Alert.alert("error", err.message || "An error occured");
+		}
+	};
+
 	return (
-		<View>
-			<Text>Login lolz</Text>
-		</View>
+		<ScrollView contentContainerStyle={styles.container}>
+			<TextInput
+				value={login.username}
+				onChangeText={(value) => setLoginInput("username", value)}
+				returnKeyType="next"
+				onSubmitEditing={() => alert(true)}
+				placeholder="Username"
+				placeholderTextColor="white"
+				style={{
+					height: 50,
+					width: "100%",
+					borderBottomWidth: 1,
+					borderColor: "green",
+					backgroundColor: "black",
+					padding: 10,
+					color: "white",
+				}}
+			/>
+			<TextInput
+				value={login.password}
+				onChangeText={(value) => setLoginInput("password", value)}
+				secureTextEntry
+				placeholder="Password"
+				placeholderTextColor="white"
+				style={{
+					height: 50,
+					width: "100%",
+					borderBottomWidth: 1,
+					borderColor: "green",
+					backgroundColor: "black",
+					padding: 10,
+					color: "white",
+				}}
+			/>
+			<Button title="Login" onPress={submit}></Button>
+		</ScrollView>
 	);
 }
